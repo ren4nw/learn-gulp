b17586e07af69982694b4076c990c33483d2f0d7
ren4nw <yung66@protonmail.com>
2020-07-27 20:20:12 -0300

    Completion of basic exercise

---
+++ basico/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp')
+const series = gulp.series
+
+function copiar(cb) {
+    // gulp.src(['pastaA/arquivo1.txt', 'pastaA/arquivo2.txt'])
+    gulp.src('pastaA/**/*.txt')
+        .pipe(gulp.dest('pastaB'))
+
+    return cb()
+}
+
+module.exports.default = series(copiar)
