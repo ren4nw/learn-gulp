fb00a8946ab81a5019c47ac3713ef8809cdaa31e
ren4nw <yung66@protonmail.com>
2020-07-28 19:05:12 -0300

    Conclusion of server task

--- spa/gulptasks/servidor.js
+++ spa/gulptasks/servidor.js
@@ -1,8 +1,14 @@
 const gulp = require('gulp')
 const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function monitorarArquivos(cb) {
-    
+    watch('src/**/*.html', () => gulp.series('appHTML')())
+    watch('src/**/*.scss', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
+
+    return cb()
 }
 
 function servidor() {
